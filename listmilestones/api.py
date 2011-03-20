0e1ce5d17f2aa35c959f1744734633353ba3724a
Mitar <mitar.git@tnode.com>
2011-03-20 13:24:59 +0100

    Added missing `ITemplateProvider` method.

--- listmilestones/api.py
+++ listmilestones/api.py
@@ -39,3 +39,6 @@ class ListMilestonesMacro(WikiMacroBase):
     def get_templates_dirs(self):
         from pkg_resources import resource_filename
         return [resource_filename(__name__, 'templates')]
+
+    def get_htdocs_dirs(self):
+        return []
